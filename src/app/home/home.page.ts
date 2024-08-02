d036698f5a636bf81015d910f3597eaeef575000
Ken Sodemann <ken.sodemann@gmail.com>
2024-08-02 09:03:24 -0500

    add problematic cases from customer

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,17 @@
 import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonListHeader,
+} from '@ionic/angular/standalone';
+import { SafeHtmlPipe } from './safe-html.pipe';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +19,11 @@ import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel
   styleUrls: ['home.page.scss'],
   standalone: true,
   imports: [
+    IonListHeader,
     IonLabel,
     IonItem,
     IonList,
+    IonListHeader,
     IonHeader,
     IonToolbar,
     IonTitle,
@@ -18,15 +31,17 @@ import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel
     NgSwitch,
     NgSwitchCase,
     NgSwitchDefault,
+    SafeHtmlPipe,
   ],
 })
 export class HomePage {
   endingHtml = '<h1>Done...</h1><p>And now my brain is fried. Oooops...</p>';
-  startingHtml = '<h1>Thinking...</h1><p>This is taking my brain a while. Hang on...</p>';
+  startingHtml =
+    '<h1>Thinking...</h1><p>This is taking my brain a while. Hang on...</p>';
   theHtml = '<h1>HTML</h1><p>This is some strange HTML</p>';
   theState = 0;
 
-  constructor() {
+  constructor(public sanitizer: DomSanitizer) {
     setTimeout(() => {
       this.theState = 1;
     }, 5000);
